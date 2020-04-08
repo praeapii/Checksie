ea22113add8788948677b4dd17fed374fee808f1
Phiset Khankhang <phisonk@hotmail.com>
2020-04-08 14:30:17 +0700

    Initial Commit

---
+++ Covid19/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid19
+//
+//  Created by Phiset Khankhang on 8/4/2563 BE.
+//  Copyright © 2563 CMKL-CIE Covid. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
